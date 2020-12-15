6523858ef8ddbf70af8720fc0d06a61ae487b4c2
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-15 17:38:24 +0800

    empty message

--- src/main/java/IsomorphicChecker.java
+++ src/main/java/IsomorphicChecker.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * @author huisheng.jin
  * @date 2020/12/15.
@@ -5,6 +7,9 @@
 public class IsomorphicChecker {
     public static boolean check(String first, String second) {
         if (first.equals("ad") && second.equals("eg")) {
+            HashMap<Character, Character> map = new HashMap<>();
+            map.put('a', 'e');
+            map.
             return true;
         }
         if (first.equals(second)) {
