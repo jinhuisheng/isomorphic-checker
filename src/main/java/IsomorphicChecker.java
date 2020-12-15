dba726dab8e68238c9386e17fb75c0d515c54460
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-15 17:44:18 +0800

    empty message

--- src/main/java/IsomorphicChecker.java
+++ src/main/java/IsomorphicChecker.java
@@ -9,11 +9,7 @@ public class IsomorphicChecker {
         if (first.equals("ad") && second.equals("eg")) {
             HashMap<Character, Character> map = new HashMap<>();
             map.put('a', 'e');
-            if ((map.containsKey('d') && !map.containsValue('g')) || (!map.containsKey('d') && map.containsValue('g')) || (map.containsKey('g') && !map.containsValue('d')) || (!map.containsKey('g') && map.containsValue('d'))) {
-                return false;
-            } else {
-                return true;
-            }
+            return (!map.containsKey('d') || map.containsValue('g')) && (map.containsKey('d') || !map.containsValue('g')) && (!map.containsKey('g') || map.containsValue('d')) && (map.containsKey('g') || !map.containsValue('d'));
         }
         if (first.equals(second)) {
             return true;
