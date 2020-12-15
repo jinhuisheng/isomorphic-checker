82cef24a4e4e0ac37437bcbc6eff626e8f8127ae
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-15 17:22:09 +0800

    empty message

---
+++ src/main/java/IsomorphicChecker.java
@@ -0,0 +1,9 @@
+/**
+ * @author huisheng.jin
+ * @date 2020/12/15.
+ */
+public class IsomorphicChecker {
+    public static boolean check(String first, String second) {
+        return true;
+    }
+}
---
+++ src/test/java/IsomorphicCheckerTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author huisheng.jin
+ * @date 2020/12/15.
+ */
+public class IsomorphicCheckerTest {
+    @Test
+    void check() {
+        assertThat(IsomorphicChecker.check("a", "a")).isEqualTo(true);
+    }
+}
