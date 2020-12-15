390dfea8245eb925ab1472fe8ef36e4dcf11a602
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-15 17:25:23 +0800

    empty message

--- src/main/java/IsomorphicChecker.java
+++ src/main/java/IsomorphicChecker.java
@@ -4,6 +4,6 @@
  */
 public class IsomorphicChecker {
     public static boolean check(String first, String second) {
-        return first.length() == second.length();
+        return true;
     }
 }
