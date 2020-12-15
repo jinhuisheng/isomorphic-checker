d97726389fa4654d5f09231934a9dc145e835b42
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-15 17:50:42 +0800

    empty message

--- src/main/java/IsomorphicChecker.java
+++ src/main/java/IsomorphicChecker.java
@@ -11,14 +11,19 @@ public class IsomorphicChecker {
             char[] firstChars = first.toCharArray();
             char[] secondChars = second.toCharArray();
             for (int i = 0; i < firstChars.length; i++) {
-
+                if (check(map, firstChars[i], secondChars[i])) {
+                    return false;
+                }
+                else {
+                    map.put(firstChars[i], secondChars[i]);
+                }
             }
-            map.put('a', 'e');
-            if (check(map, 'd', 'g')) {
-                return false;
-            } else {
-                return true;
-            }
+//            map.put('a', 'e');
+//            if (check(map, 'd', 'g')) {
+//                return false;
+//            } else {
+//                return true;
+//            }
         }
         if (first.equals(second)) {
             return true;
